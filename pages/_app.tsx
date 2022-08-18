835352172ac6d25dae7a33fca0a0b21473a72c01
marianomurad <marianomurad21@gmail.com>
2022-08-18 00:41:15 -0300

    added chakra & prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
 import LayoutComponent from "../app/components/Layout";
+import { ChakraProvider } from '@chakra-ui/react'
 import '../app/styles/globals.css'
 import 'tailwindcss/tailwind.css'
 
 
 function CustomApp ({Component, pageProps}) {
-    return (
-        <>
-        <LayoutComponent>
-            <Component {...pageProps}/>
-        </LayoutComponent>
-  </>
-    );
-}
+    return (<ChakraProvider>
+            <LayoutComponent>
+                <Component {...pageProps}/>
+            </LayoutComponent>
+        </ChakraProvider>);
+}  
 
 export default CustomApp;
+
